2876d02114884ad70f609e9aa443368c52664681
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2024-01-25 18:56:15 -0600

    cesar done

---
+++ parte_1/ceaser.py
@@ -0,0 +1,64 @@
+'''
+Universidad del valle de Guatemala
+25-01-2024
+author: Marco Jurado 20308
+Cifrado de información
+
+ceaser.py
+'''
+
+
+'''
+x (str): String to check and remove for special characters.
+
+Returns: clean string with no special characters.
+'''
+def no_special_signs(x):
+    special_signs = {
+        'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u',
+        'Á': 'A', 'É': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U',
+        'ü': 'u', 'Ü': 'U'
+    }
+
+    return ''.join(special_signs.get(letra, letra) for letra in x)
+
+'''
+x (str): String to be encrypted.
+y (int): Integer of the displacement applied for encryption.
+
+Returns: encrypted message.
+'''
+def simple_ceaser_encrypt(x,y):
+    alfabeto = 'abcdefghijklmnñopqrstuvwxyz'
+    x = no_special_signs(x.lower())
+    result = ''
+
+    for letra in x:
+        if letra in alfabeto:
+            posicion = (alfabeto.index(letra) + y) % len(alfabeto)
+            result = result + alfabeto[posicion]
+
+        else:
+            result = result + letra
+
+    return result
+
+'''
+x (str): String to be decrypted.
+y (int): Integer of the displacement applied for encryption.
+
+Returns: decrypted message.
+'''
+def simple_ceaser_decrypt(x, y):
+    alfabeto = 'abcdefghijklmnñopqrstuvwxyz'
+    x = x.lower()
+    result = ''
+
+    for letra in x:
+        if letra in alfabeto:
+            posicion = (alfabeto.index(letra) - y) % len(alfabeto)
+            result = result + alfabeto[posicion]
+        else:
+            result = result + letra
+
+    return result
